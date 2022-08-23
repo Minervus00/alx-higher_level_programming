5e9bb524dc2328d5c6911ca720be3f379c4e33ea
Minervus00 <fayouedraogo@protonmail.com>
2022-08-23 09:06:13 -0700

    fun

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for c in "abcdefghijklmnopqrstuvwxyz"[::-1]:
-    print(chr(ord(c) - 32) if ord(c) % 2 != 0 else c, end="")
+    print("{}".format(chr(ord(c) - 32) if ord(c) % 2 != 0 else c), end="")
