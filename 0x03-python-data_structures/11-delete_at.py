471f40717dea7fe49f6804b7a7712ee9668d1c21
Minervus00 <fayouedraogo@protonmail.com>
2022-08-27 08:09:43 -0700

    fun

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,5 +2,5 @@
 def delete_at(my_list=[], idx=0):
     n = len(my_list)
     if 0 <= idx < n:
-        return [my_list[x] for x in range(n) if x != idx]
+        my_list[:] = [my_list[x] for x in range(n) if x != idx]
     return my_list
