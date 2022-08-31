951ff15c3f02f664921a32860455615b49680e6b
Minervus00 <fayouedraogo@protonmail.com>
2022-08-31 10:08:51 -0700

    fun

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    n = len(my_list)
+    if n == 0:
+        return 0
+    som, div = 0, 0
+    for i, j in my_list:
+        som += ( i * j)
+        div += j
+    return som / div
