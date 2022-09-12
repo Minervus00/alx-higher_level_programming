e304ac5b914fdba20de5b11ec73678a4c6213470
Minervus00 <fayouedraogo@protonmail.com>
2022-09-12 15:32:07 +0000

    fun

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as vaerr:
+        print(f"Exception: {vaerr}", file=stderr)
+        return False
+    except TypeError as tyerr:
+        print(f"Exception: {tyerr}", file=stderr)
+        return False
+    return True
