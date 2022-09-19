109226195a433456e1669747a67c0d33254c5b2e
Minervus00 <fayouedraogo@protonmail.com>
2022-09-19 16:23:45 +0000

    fun

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -26,7 +26,7 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for num in line:
             if not isinstance(num, (int, float)):
-                raise TypeError(msg)  
+                raise TypeError(msg)
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
