3b2c65bc66d52019ca3320cb8f7a9944d329c54d
Minervus00 <fayouedraogo@protonmail.com>
2022-09-19 21:37:59 +0000

    fun

---
+++ 0x08-python-more_classes/main.py
@@ -0,0 +1,6 @@
+#!C:/Users/LENOVO/AppData/Local/Programs/Python/Python39/python.exe
+Rectangle = __import__('0-rectangle').Rectangle
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
