b56275429e875f9dc62895af92eb89f5d17ab9ff
Minervus00 <fayouedraogo@protonmail.com>
2022-09-22 13:08:09 +0000

    funky

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module for the Queen"""
 from sys import exit, argv, setrecursionlimit
-setrecursionlimit(15000)
+setrecursionlimit(5000)
 # ================ Handle errors ========
 
 
