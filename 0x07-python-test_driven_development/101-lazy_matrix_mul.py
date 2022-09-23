1ec9c8cf062b1165e61bc688c2abe8ce8fff31ac
Minervus00 <fayouedraogo@protonmail.com>
2022-09-23 18:10:01 +0000

    fun

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,6 +7,4 @@ import numpy as np
 def lazy_matrix_mul(m_a, m_b):
     """Returns the product of two matrices using numpy"""
 
-    m_a = np.array(m_a)
-    m_b = np.array(m_b)
     return np.matmul(m_a, m_b)
