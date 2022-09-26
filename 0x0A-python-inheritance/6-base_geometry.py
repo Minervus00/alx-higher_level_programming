9d53ad940f6968efaa1342893762d37494fe8408
Minervus00 <fayouedraogo@protonmail.com>
2022-09-26 17:21:32 +0000

    fun

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module containing class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+
+    def area(self):
+        """area() an public instance method"""
+        raise Exception("area() is not implemented")
