086c04c1a636a8356187811c91482a408028f09d
Minervus00 <fayouedraogo@protonmail.com>
2022-09-26 17:16:03 +0000

    fun

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module containing inherits_from"""
+
+
+class BaseGeometry:
+    pass
--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,10 +1,8 @@
 #!C:/Users/LENOVO/AppData/Local/Programs/Python/Python39/python.exe
-inherits_from = __import__('4-inherits_from').inherits_from
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
