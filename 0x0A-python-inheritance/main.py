afb731e22bea4fbca23d580a4f0ec51172568ac0
Minervus00 <fayouedraogo@protonmail.com>
2022-09-27 10:21:10 +0000

    fun

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,8 +1,10 @@
 #!C:/Users/LENOVO/AppData/Local/Programs/Python/Python39/python.exe
 add_attribute = __import__('101-add_attribute').add_attribute
 
+
 class MyClass():
     pass
+
 
 mc = MyClass()
 add_attribute(mc, "name", "John")
