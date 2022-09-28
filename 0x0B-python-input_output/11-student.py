91c6ec87bde900c0c2dbe9e82f89e9a31c9f3859
Minervus00 <fayouedraogo@protonmail.com>
2022-09-28 20:27:16 +0000

    fun

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module containing class Student"""
+
+
+class Student:
+    """Defines a student by first_name, last_name and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """__init__ docu"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        dic = {}
+        if attrs is not None:
+            for ky, val in self.__dict__.items():
+                if ky in attrs:
+                    dic[ky] = val
+            return dic
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for ky, val in json.items():
+            setattr(self, ky, val)
