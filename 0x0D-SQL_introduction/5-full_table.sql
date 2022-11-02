91387344c224d36e2fbb6142347d3e8bd4371c74
Minervus00 <fayouedraogo@protonmail.com>
2022-11-02 00:23:25 +0000

    funky

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,4 @@
 -- Prints the full description of the table first_table from the database hbtn_0c_0
 -- cat ./5-* | mysql -u root -p hbtn_0c_0
+-- This statement displays thequery used to create the table
 SHOW CREATE TABLE first_table;
