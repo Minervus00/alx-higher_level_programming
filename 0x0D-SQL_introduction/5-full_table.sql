500ba62af935433eeac96457af4e1d9c81436948
Minervus00 <fayouedraogo@protonmail.com>
2022-11-02 00:05:06 +0000

    funky

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Prints the full description of the table first_table from the database hbtn_0c_0
+-- cat ./5-* | mysql -u root -p hbtn_0c_0
+SHOW TABLE STATUS;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- Lists all rows of the table first_table from the database hbtn_0c_0
+-- cat ./6-* | mysql -u root -p hbtn_0c_0
+SELECT * FROM first_table;
