fb9761c8c538f7a3b0fa71ae2655779333e5a724
Minervus00 <fayouedraogo@protonmail.com>
2022-11-29 11:07:24 +0000

    funky

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const argtab = process.argv;
+let maxi = 0;
+let maxi2 = 0;
+let tmp = 0;
+let k = 2;
+
+for (; argtab[k]; k++) {
+  tmp = parseInt(argtab[k]);
+  if (tmp > maxi) {
+    maxi2 = maxi;
+    maxi = tmp;
+  } else if (tmp > maxi2 && tmp !== maxi) {
+    maxi2 = tmp;
+  }
+}
+
+if (k === 3) {
+  console.log(0);
+} else {
+  console.log(maxi2);
+}
