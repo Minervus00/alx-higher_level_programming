66294ccacade81657a0463b41d21514997e70d7f
Minervus00 <fayouedraogo@protonmail.com>
2022-12-03 20:56:54 +0000

    funky

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!C:/Users/LENOVO/anaconda3/python.exe
+# /usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
