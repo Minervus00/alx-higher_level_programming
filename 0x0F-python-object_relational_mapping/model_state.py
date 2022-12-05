551a825c4f4c05680dcd79ebbedaee0a7b27c3df
Minervus00 <fayouedraogo@protonmail.com>
2022-12-05 14:27:31 +0000

    funky

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """State function docu"""
     __tablename__ = 'states'
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
