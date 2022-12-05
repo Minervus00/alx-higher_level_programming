c534f97545a2acd8388b8107b3812e98900159df
Minervus00 <fayouedraogo@protonmail.com>
2022-12-05 19:27:57 +0000

    funky

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -7,8 +7,8 @@ Base = declarative_base()
 
 
 class City(Base):
-    """State function docu"""
-    __tablename__ = 'cities'
-    id = Column(Integer, nullable=False, primary_key=True)
+    """City function docu"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
