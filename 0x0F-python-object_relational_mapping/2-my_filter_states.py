6b00af172f5d2df2e170de9721d771853a2e289e
Minervus00 <fayouedraogo@protonmail.com>
2022-12-05 12:40:50 +0000

    funky

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute(
-        "SELECT * FROM states WHERE name='{}' ORDER BY id".format(argv[4]))
+        "SELECT * FROM states WHERE BINARY name='{}' ORDER BY id".format(argv[4]))
 
     rows = cur.fetchall()
 
