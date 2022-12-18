23c51b740e51040352060143664645a0213654f3
Minervus00 <fayouedraogo@protonmail.com>
2022-12-18 22:54:26 +0000

    funky_

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI $1 | head -1 | cut -d ' ' -f 2
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" $1
