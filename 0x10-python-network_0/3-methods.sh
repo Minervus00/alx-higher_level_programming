af8555934bc8b0270a3e69f7c6be525712d2d793
Minervus00 <fayouedraogo@protonmail.com>
2022-12-18 22:28:15 +0000

    funky_

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL and displays all HTTP methods the server will accept
+# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 curl -sI $1 | grep -Fi Allow | cut -d ' ' -f 2-
