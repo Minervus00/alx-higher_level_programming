3f03ede2e648a14e27b4c70027cebd170a3fb78e
Minervus00 <fayouedraogo@protonmail.com>
2022-12-19 17:12:17 +0000

    funky_

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """ takes in a URL, sends a request to the URL and displays the body of the
-    response (decoded in utf-8) """
+    response (decoded in utf-8)
+    - Handles HTTP errors"""
 
 if __name__ == "__main__":
     import urllib.request
     import urllib.error
     from sys import argv
 
+    req = urllib.request.Request(argv[1])
     try:
-        with urllib.request.urlopen(argv[1]) as resp:
+        with urllib.request.urlopen(req) as resp:
             print(resp.read().decode('utf-8'))
     except urllib.error.HTTPError as err:
         print("Error code:", err.code)
