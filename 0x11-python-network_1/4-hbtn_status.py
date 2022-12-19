dd43b6a0f195bbd680f2eb58bffa7f3a60460e96
Minervus00 <fayouedraogo@protonmail.com>
2022-12-19 16:46:04 +0000

    funky_

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!C:/Users/LENOVO/anaconda3/python.exe
+# /usr/bin/python3
 """ takes in a URL, sends a request to the URL and displays the body of the
     response (decoded in utf-8) """
 
