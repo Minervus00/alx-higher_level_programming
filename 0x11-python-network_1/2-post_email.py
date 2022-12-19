5959c145a49121406c841beea3045b068d2e64f6
Minervus00 <fayouedraogo@protonmail.com>
2022-12-19 16:31:45 +0000

    funky_

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
 
     req = urllib.request.Request(argv[1], data)
     with urllib.request.urlopen(req) as resp:
-        print(resp.decode('utf-8'))
+        print(resp.read().decode('utf-8'))
