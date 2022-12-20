36d505006a8c31703ef9a786ade38134307f76b7
Minervus00 <fayouedraogo@protonmail.com>
2022-12-20 11:28:02 +0000

    funky_

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
-""" takes in a letter and sends a POST request to
-    http://0.0.0.0:5000/search_user with the letter as a parameter.
-    + The letter must be sent in the variable q
-    + If no argument is given, set q=""
-    + If the response body is properly JSON formatted and not empty, display
-    the id and name like this: [<id>] <name>
-    + Otherwise:
-        * Display Not a valid JSON if the JSON is invalid
-        * Display No result if the JSON is empty"""
+""" that takes 2 arguments (repo_name and owner) in order to print all commits
+    by: <sha>: <author name>` (one by line)"""
 
 if __name__ == "__main__":
     import requests
     from sys import argv
 
-    q = ""
-    if len(argv) > 1:
-        q = argv[1]
-    resp = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
-
-    try:
-        json_r = resp.json()
-        if not json_r:
-            print("No result")
-        else:
-            print("[{}] {}".format(json_r['id'], json_r['name']))
-    except ValueError:
-        print("Not a valid JSON")
+    resp = requests.get(
+        f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits")
+    rep = resp.json()
+    for coms in rep[:10]:
+        print(f"{coms['sha']}: {coms['commit']['author']['name']}")
