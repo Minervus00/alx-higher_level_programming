abc04e9cde684b057c8adfeab314e7bb3c14819f
Minervus00 <fayouedraogo@protonmail.com>
2022-12-20 09:34:01 +0000

    funky_

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-""" takes in a URL and an email address, sends a POST request to the passed
-    URL with the email as a parameter, and finally displays the body of the
-    response """
+""" takes in a URL, sends a request to the URL and displays the body
+    of the response.
+    + handles errors"""
 
 if __name__ == "__main__":
     import requests
     from sys import argv
 
-    resp = requests.post(argv[1], data={'email': argv[2]})
-    print(resp.text)
+    resp = requests.get(argv[1])
+    if resp.status_code < 400:
+        print(resp.text)
+    else:
+        print("Error code:", resp.status_code)
