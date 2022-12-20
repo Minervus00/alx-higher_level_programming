c2c45b62798476f3451017fb2426a1b9b5889801
Minervus00 <fayouedraogo@protonmail.com>
2022-12-20 11:07:27 +0000

    funky_

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,4 @@
-#!C:/Users/LENOVO/anaconda3/python.exe
-# /usr/bin/python3
+#!/usr/bin/python3
 """ takes your GitHub credentials (username and password) and uses the
     GitHub API to display your id"""
 
