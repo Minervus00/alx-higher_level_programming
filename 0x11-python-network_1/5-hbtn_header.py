0d8d55bd5eb56cf55f1a00ab86365767a0102a54
Minervus00 <fayouedraogo@protonmail.com>
2022-12-20 09:11:37 +0000

    funky_

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,4 @@
-#!C:/Users/LENOVO/anaconda3/python.exe
-# /usr/bin/python3
+#!/usr/bin/python3
 """ takes in a URL, sends a request to the URL and displays the value of the
     variable X-Request-Id in the response header """
 
