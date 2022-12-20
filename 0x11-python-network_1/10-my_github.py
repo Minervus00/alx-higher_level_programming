99013e1840c9dd39b582fbfdd95c1416082d043b
Minervus00 <fayouedraogo@protonmail.com>
2022-12-20 11:35:50 +0000

    funky_

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ takes your GitHub credentials (username and password) and uses the
     GitHub API to display your id"""
+# https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#get-the-authenticated-user
 
 if __name__ == "__main__":
     import requests
@@ -10,4 +11,5 @@ if __name__ == "__main__":
     resp = requests.get(
         f"https://api.github.com/users/{argv[1]}", headers=header)
     # https://api.github.com/users/<username>
+
     print(resp.json().get('id'))
--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ that takes 2 arguments (repo_name and owner) in order to print all commits
     by: <sha>: <author name>` (one by line)"""
+# https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28
 
 if __name__ == "__main__":
     import requests
@@ -9,6 +10,7 @@ if __name__ == "__main__":
     resp = requests.get(
         f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits")
     # https://api.github.com/repos/<owner_name>/<repo_name>
+
     rep = resp.json()
     for coms in rep[:10]:
         print(f"{coms['sha']}: {coms['commit']['author']['name']}")
