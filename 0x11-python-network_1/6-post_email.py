04244a430986c3cea2fb66d4c57bb13ea283d6c7
Minervus00 <fayouedraogo@protonmail.com>
2022-12-20 01:23:40 -0800

    funky_

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
-    resp = requests.post(argv[1], params={'email': argv[2]})
+    resp = requests.post(argv[1], data={'email': argv[2]})
     print(resp.text)
