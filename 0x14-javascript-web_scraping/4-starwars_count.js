98e182a1ba860928a9b33e0845026b2cafd6046c
Minervus00 <fayouedraogo@protonmail.com>
2023-01-18 13:56:27 +0000

    funky

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/people/18'
+const url = 'https://swapi-api.alx-tools.com/api/people/18';
 
 request(url, (error, response, body) => {
   if (error) throw error;
