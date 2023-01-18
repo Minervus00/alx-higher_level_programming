1a9ac1467f3afdfdab614a622c888e0af2a9225c
Minervus00 <fayouedraogo@protonmail.com>
2023-01-18 13:58:58 +0000

    funky

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/people/18'
 
 request(url, (error, response, body) => {
   if (error) throw error;
-  console.log(JSON.parse(body).title);
+  console.log(JSON.parse(body).films.length);
 });
