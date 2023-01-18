f29fdb7f040e80bec95bc8e529c5b5e1f2ac8012
Minervus00 <fayouedraogo@protonmail.com>
2023-01-18 11:01:00 +0000

    funky

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const url = process.argv[2];
 
 request(url, (error, response) => {
   if (error) throw error;
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });
