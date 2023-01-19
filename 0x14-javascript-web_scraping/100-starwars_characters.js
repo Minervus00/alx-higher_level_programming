81b1e625cc1d90d6c3cb59e7d19649f209e8863a
Minervus00 <fayouedraogo@protonmail.com>
2023-01-19 00:03:45 +0000

    funky

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,23 +2,18 @@
 // Prints all characters of a Star Wars movie which id is given as parameter 
 
 const request = require('request');
-const mvId = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/';
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, (error, response, body) => {
   if (!error) {
-    const result = JSON.parse(body).results;
+    const result = JSON.parse(body).characters;
 
-    result.forEach(element => {
-      if (element.episode_id.toString() === mvId) {
-        element.characters.forEach(char => {
-          request(char, (err, resp, bdy) => {
-            if (!err) {
-              console.log(JSON.parse(bdy).name);
-            }
-          });
-        });
-      }
+    result.forEach(char => {
+      request(char, (err, resp, bdy) => {
+        if (!err) {
+          console.log(JSON.parse(bdy).name);
+        }
+      });
     });
   }
 });
