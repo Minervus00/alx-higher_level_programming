be52e6cc1e4008efb3388727dff54a8db8e99200
Minervus00 <fayouedraogo@protonmail.com>
2023-01-26 10:06:53 +0000

    fun

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,7 +1,7 @@
 const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
 
-$(function () {
-  $.get(url, function (data, textStatus) {
+$('document').ready(function () {
+  $.get(url, function (data) {
     $('DIV#hello').text(data.hello);
   });
 });
