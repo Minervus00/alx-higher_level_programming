eb687664f2920a95be6bf796973fed00b5a687c2
Minervus00 <fayouedraogo@protonmail.com>
2023-01-28 13:39:55 +0000

    fun

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,9 +1,4 @@
-//Some stuff.
-var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-3.2.1.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
-
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
   const para = document.querySelector('header');
   para.style.color = '#FF0000';
 });
