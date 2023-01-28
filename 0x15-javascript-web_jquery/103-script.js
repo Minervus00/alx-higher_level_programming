f75661deaabf7f0bfc5c62e3ae7da85f3a52795f
Minervus00 <fayouedraogo@protonmail.com>
2023-01-28 16:26:29 +0000

    funky

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -2,14 +2,25 @@ $('document').ready(function () {
   const lng = document.getElementById('language_code').value;
   const url = 'https://fourtonfish.com/hellosalut/?lang=' + lng;
 
-  $(document).keypress(function(e) {
-    var keycode = (e.keyCode ? e.keyCode : e.which);
-    if (keycode == '13') {
-      $.get(url, function (data) {
-        $('DIV#hello').text(data.hello);
-      });
+  var input = document.getElementById("language_code");
+
+  // Execute a function when the user presses a key on the keyboard
+  input.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Trigger the button element with a click
+      document.getElementById("btn_translate").click();
     }
   });
+
+  // $(document).keypress(function(e) {
+  //   var keycode = (e.keyCode ? e.keyCode : e.which);
+  //   if (keycode == '13') {
+  //     $.get(url, function (data) {
+  //       $('DIV#hello').text(data.hello);
+  //     });
+  //   }
+  // });
 
   $('INPUT#btn_translate').click(function () {
     $.get(url, function (data) {
